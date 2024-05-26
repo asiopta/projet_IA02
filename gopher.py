c45f36cb0ee985126cd85ebe38b85bd957ceed0b
ismataboukhachfe <132674926+ismataboukhachfe@users.noreply.github.com>
2024-05-26 13:42:20 +0200

    LegalsGOPHER

--- gopher.py
+++ gopher.py
@@ -1,20 +1,65 @@
 from typing import Union
 from fonctionscommunes import *
 
-print(fc.empty_state(7))
+#print(empty_state(7))
 
-def legals_Gopher(state:grid,Player:player) -> list[Cell] : 
-    friendly = [] 
-    ennemy = []
+
+
+def one_ennemy_connection(cell: Cell,player:Player, grid : State) -> bool  : 
+    
+    State = state_to_environnement(grid)
+    voisins = voisins(cell,State)
+    nb_ennemies_connection = 0
+    
+    for i in voisins : 
+        
+        if State[i] != Player and State[i] != 0 : 
+            
+            nb_ennemies_connection = nb_ennemies_connection + 1
+    
+    if nb_ennemies_connection == 1 : return True
+    return False
+
+
+def friendy_connection(cell: Cell,player:Player, grid : State) -> bool  : 
+    State =  state_to_environnement(grid)
+    voisins = voisins(cell,grid)
+    
+    for i in voisins : 
+        
+        if State[i] == Player :  return True
+    
+    return False
+            
+
+def legals_Gopher(grid:State,player:Player) -> list[Cell] : 
+    
+    state =  state_to_environnement(grid)
+
+    vide = True
+    for i,j in state.items() : 
+         if j != 0 : vide = False
+    
+    if vide :
+        return list(state.keys())
+    
     legals = []
-    for i,j in grid.enumerate() : 
-         if j == player : 
-             friendly.append(i)
-             ennemy.append(i)
-         if j != player and j!= 0 : 
-             ennemy.append(i)
-         
-    if len(ennemy) == 0 and len(friendly) == 0 :
+    
+    for i,j in state.enumerate() : 
+        
+        if not friendy_connection(i,player,state) and  one_ennemy_connection(i,player,state) : 
+            legals.append(i)
+    return legals
+
+ 
+print(legals_Gopher(empty_state(7),1))
+            
+            
+            
+            
+            
+            
+            
         
         
 
