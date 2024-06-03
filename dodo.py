7294b8742bc7d0c0954c2b0064eaf9def6c7a50b
asiopta <128475295+asiopta@users.noreply.github.com>
2024-06-03 13:25:26 +0200

    Update dodo.py

--- dodo.py
+++ dodo.py
@@ -32,19 +32,20 @@ def initial_state_dodo() -> State:
     result.append(((-2, 1), 0))
     result.append(((1, -2), 0))
 
-    result.append(((0,0),0))
+    result.append(((0, 0), 0))
     # resultat
+    result = sorted(result, key=lambda x: x[0][0] + x[0][1], reverse=True)
     return result
 
 
-#print(initial_state_dodo()) 
+# print(initial_state_dodo())
 
 def voisins_Dodo(cellule_dodo: tuple[Cell, Player], grid: Environment) -> list[Cell]:
-    result: List[Cell] = []
+    result: list[Cell] = []
     cell = cellule_dodo[0]
-    
+
     if cellule_dodo[1] == 1:
-    #  print("HELLO")
+        #  print("HELLO")
         new_cells = [
             (cell[0], cell[1] + 1),  # haut a gauche
             (cell[0] + 1, cell[1]),  # Haut a droite
@@ -56,22 +57,24 @@ def voisins_Dodo(cellule_dodo: tuple[Cell, Player], grid: Environment) -> list[C
             (cell[0] - 1, cell[1]),  # en bas a droite
             (cell[0] - 1, cell[1] - 1)  # directement au dessous
         ]
-    
-  #  print(new_cells)
-    for new_cell in new_cells:
-        
-       # print(new_cell)
-     #  print(grid)
-        for cellplayer in grid :  
-           #print("okay")
-            if new_cell == cellplayer  :
-                result.append(new_cell)
-  # print(result)
-    return result
-#print(initial_state_dodo())
-#rint(state_to_environnement(initial_state_dodo()))
 
-#rint(voisins_Dodo(((1,0),2),state_to_environnement(initial_state_dodo())))
+    #  print(new_cells)
+    for new_cell in new_cells:
+
+        # print(new_cell)
+        #  print(grid)
+        for cellplayer in grid:
+            # print("okay")
+            if new_cell == cellplayer:
+                result.append(new_cell)
+    # print(result)
+    return result
+
+
+# print(initial_state_dodo())
+# rint(state_to_environnement(initial_state_dodo()))
+
+# rint(voisins_Dodo(((1,0),2),state_to_environnement(initial_state_dodo())))
 
 def voisins_libres_dodo(cellule_dodo: tuple[Cell, Player], grid: Environment) -> list[Cell]:
     voisins = voisins_Dodo(cellule_dodo, grid)
@@ -81,11 +84,12 @@ def voisins_libres_dodo(cellule_dodo: tuple[Cell, Player], grid: Environment) ->
             result.append(cell)
     return result
 
+
 def legals_dodo(state: State, tour: Player) -> list[ActionDodo]:
     result: list[ActionDodo] = []
     grid: Environment = state_to_environnement(state)
 
-    # Lescellules appartenant au joueur 
+    # Lescellules appartenant au joueur
     player_cells = [cell for cell in state if cell[1] == tour]
     for cell in player_cells:
         voisins = voisins_libres_dodo(cell, grid)
@@ -98,12 +102,15 @@ def legals_dodo(state: State, tour: Player) -> list[ActionDodo]:
 def final_dodo(state: State, tour: Player) -> bool:
     return legals_dodo(state, tour) == []
 
+
+###A VERIFIER LE RESTE ENCORE
 def score_dodo(state: State, tour: Player) -> int:
     if final_dodo(state, tour):
         if tour == 1:
             return 1
         else:
             return -1
+
 
 def play_dodo(state: State, action: ActionDodo, tour: Player) -> State:
     if action not in legals_dodo(state, tour):
@@ -114,7 +121,7 @@ def play_dodo(state: State, action: ActionDodo, tour: Player) -> State:
         grid[action[0]] = 0
         grid[action[1]] = tour
     return environnement_to_state(grid)
-         
+
 
 def evaluation_state_dodo(state: State, tour: Player) -> float:  # à faire
     """fonction d'évaluation de l'état d'un jeu DODO"""
@@ -125,10 +132,9 @@ def evaluation_state_dodo(state: State, tour: Player) -> float:  # à faire
     elif nb_legals_adversaire == 0:
         return -1
     else:
-        diff_int =  nb_legals_adversaire - nb_legals_player
-        return diff_int /(nb_legals_adversaire + nb_legals_player)
+        diff_int = nb_legals_adversaire - nb_legals_player
+        return diff_int / nb_legals_adversaire + nb_legals_player
 
-###A VERIFIER LE RESTE ENCORE 
 
 # stratégie alpha-beta
 def alphabeta_dodo(state: State, tour: Player, alpha: float = -1000, beta: float = 1000) -> float:
@@ -182,6 +188,10 @@ def alphabeta_dodo_depth(state: State, tour: Player, depth: int, alpha: float, b
             return value
 
 
+maximizing_player: Player = 1
+minimizing_player: Player = 2
+
+
 @memoize
 def alphabeta_action_dodo(state: State, tour: Player, alpha=-100, beta=100) -> tuple[float, ActionDodo]:
     """alphabeta avec action avec depth illimité"""
@@ -211,20 +221,22 @@ def alphabeta_action_dodo(state: State, tour: Player, alpha=-100, beta=100) -> t
 def alphabeta_action_dodo_depth(state: State, tour: Player, alpha=-100, beta=100) -> tuple[float, ActionDodo]:
     """alphabeta avec action avec depth limité"""
     if final_dodo(state, tour):
-        return score_dodo(state, tour), (4, 4)
+        return score_dodo(state, tour), ((4, 4), (4, 4))
     if tour == maximizing_player:
-        best_action: Action = legals_dodo(state, tour)[0]
+        actions_legales: list[ActionDodo] = legals_dodo(state, tour)
+        best_action: Action = actions_legales[0]
         best_score: float = -10000
-        for action in legals_dodo(state, tour):
-            bla = alphabeta_dodo_depth(play_dodo(state, action, tour), minimizing_player, 10, alpha, beta)
+        for action in actions_legales:
+            bla = alphabeta_dodo_depth(play_dodo(state, action, tour), minimizing_player, 3, alpha, beta)
             if bla > best_score:
                 best_score = bla
                 best_action = action
     else:
-        best_action: Action = legals_dodo(state, tour)[0]
+        actions_legales: list[ActionDodo] = legals_dodo(state, tour)
+        best_action: Action = actions_legales[0]
         best_score: float = 10000
-        for action in legals_dodo(state, tour):
-            bla = alphabeta_dodo_depth(play_dodo(state, action, tour), maximizing_player, 10, alpha, beta)
+        for action in actions_legales:
+            bla = alphabeta_dodo_depth(play_dodo(state, action, tour), maximizing_player, 3, alpha, beta)
             if bla < best_score:
                 best_score = bla
                 best_action = action
@@ -233,8 +245,9 @@ def alphabeta_action_dodo_depth(state: State, tour: Player, alpha=-100, beta=100
 
 
 def strategy_alphabeta_dodo(state: State, tour: Player) -> ActionDodo:
-    best_score, best_action = alphabeta_action_dodo(state, tour)
+    best_score, best_action = alphabeta_action_dodo_depth(state, tour)
     return best_action
+
 
 def strategy_random_dodo(state: State, tour: Player) -> ActionDodo:
     legal_actions: list[ActionDodo] = legals_dodo(state, tour)
@@ -247,22 +260,18 @@ def dodo(strategy_X: Strategy, strategy_O: Strategy) -> Score:
     while not final_dodo(state, 1):
         action_1: Action = strategy_X(state, 1)
         state = play_dodo(state, action_1, 1)
+        print("----------------joueur1-----------------------")
+        print(action_1)
+        print()
         pprint(state, 4)
+
         if not final_dodo(state, 2):
             action_2: Action = strategy_O(state, 2)
             state = play_dodo(state, action_2, 2)
+            print("----------------joueur2-----------------------")
+            print(action_2)
+            print()
             pprint(state, 4)
         else:
             return score_dodo(state, 2)
     return score_dodo(state, 1)
-
-
-pprint(initial_state_dodo(),4)
-
-
-
-
-
-
-
-
